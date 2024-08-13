193e362236973bbc00e0ffdc598b157fd8ae3784
Jean Flaherty <kobejean@me.com>
2024-08-14 06:20:16 +0900

    m

--- .verify-helper/cache/test/pow_of_matrix.test.py
+++ .verify-helper/cache/test/pow_of_matrix.test.py
@@ -1,26 +1,6 @@
 # verification-helper: PROBLEM https://judge.yosupo.jp/problem/pow_of_matrix
 
-
-def mat_mul(A,B,mod):
-    assert len(A[0]) == len(B)
-    R = [[0]*len(B[0]) for _ in range(len(A))] 
-    for i,Ri in enumerate(R):
-        for k,Aik in enumerate(A[i]):
-            for j,Bkj in enumerate(B[k]):
-                Ri[j] = (Ri[j] + Aik*Bkj) % mod  
-    return R 
-
-def mat_id(N):
-    return [[int(i==j) for j in range(N)] for i in range(N)]
-
-def mat_pow(A,K,mod):
-    N = len(A)
-    ret = A if K & 1 else mat_id(N)
-    for i in range(1,K.bit_length()):
-        A = mat_mul(A,A,mod) 
-        if K >> i & 1:
-            ret = mat_mul(ret,A,mod) 
-    return ret 
+from cp_library.math.mod.matpow import mat_pow
 
 mod = 998244353
 
--- test_all.sh
+++ test_all.sh
@@ -4,11 +4,14 @@
 mkdir -p .verify-helper/cache/test
 
 CXXFLAGS="-isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk -ftree-vectorize -fPIC -fstack-protector-strong -O2 -pipe -fvisibility-inlines-hidden -fmessage-length=0"
+for test_file in test/*.test.py; do
+    cp "$test_file" ".verify-helper/cache/$test_file"
+    oj-bundle ".verify-helper/cache/$test_file" > "$test_file"
+done
+
+oj-verify all
 
 for test_file in test/*.test.py; do
-    filename=$(basename "$test_file")
-    oj-bundle "$test_file" > ".verify-helper/cache/test/$filename"
+    rm "$test_file"
+    cp  ".verify-helper/cache/$test_file" "$test_file"
 done
-cd .verify-helper/cache/test
-oj-verify all
-cp -r .verify-helper/* ../../
