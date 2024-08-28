258986d85da215298d42b56d6b6fb502a8dd937b
Jean Flaherty <kobejean@me.com>
2024-08-29 07:36:44 +0900

    l

--- cp_library/alg/dp/rerooting.py
+++ cp_library/alg/dp/rerooting.py
@@ -2,37 +2,32 @@ import cp_library.misc.setrecursionlimit
 from cp_library.ds.bidirectional_array import BidirectionalArray
 
 class ReRootingDP:
-    def __init__(self, G, e, merge, add_node) -> None:
-        self.G = G
+    def __init__(self, T, e, merge, add_node) -> None:
+        self.T = T
         self.e = e
         self.merge = merge
         self.add_node = add_node
-        self.dp = None
-        self.state = None
-        self.ans = None
-
-    def dfs_up(self,u):
-        self.state[u] = 1
-        res = self.e
-        for i,v in enumerate(self.G[u]):
-            if self.state[v] == 0:
-                self.dp[u][i] = self.dfs_up(v)
-                res = self.merge(res, self.dp[u][i])
-        return self.add_node(u, res)
-    
-    def dfs_down(self,u):
-        self.state[u] = 2
-        ba = BidirectionalArray(self.e, self.merge, self.dp[u])
-        for i,v in enumerate(self.G[u]):
-            if self.state[v] == 1:
-                self.dp[v][self.G[v].index(u)] = self.add_node(u, ba.out(i))
-                self.dfs_down(v)
-        self.ans[u] = ba.all()
 
     def solve(self):
-        self.dp = [[self.e]*len(adj) for adj in self.G]
-        self.state = [0 for _ in range(len(self.G))]
-        self.ans = [self.e for _ in range(len(self.G))]
-        self.dfs_up(0)
-        self.dfs_down(0)
-        return self.ans
+        dp = [[self.e]*len(adj) for adj in self.T]
+        ans = [None for _ in range(len(self.T))]
+        
+        def dfs_up(u, p=None):
+            res = self.e
+            for i,v in enumerate(self.T[u]):
+                if v != p:
+                    dp[u][i] = dfs_up(v,u)
+                    res = self.merge(res, dp[u][i])
+            return self.add_node(u, res)
+        
+        def dfs_down(u, p=None):
+            ba = BidirectionalArray(self.e, self.merge, dp[u])
+            for i,v in enumerate(self.T[u]):
+                if v != p:
+                    dp[v][self.T[v].index(u)] = self.add_node(u, ba.out(i))
+                    dfs_down(v,u)
+            ans[u] = ba.all()
+        
+        dfs_up(0)
+        dfs_down(0)
+        return ans
