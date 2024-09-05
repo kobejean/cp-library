f4994b1ac6b6469361565f13526b3d04297c36cd
Jean Flaherty <kobejean@me.com>
2024-09-05 11:18:10 +0900

    m

---
+++ cp_library/vis/plot_graph_weighted_fn.py
@@ -0,0 +1,32 @@
+
+def plot_graph(G, N):
+    import networkx as nx
+    import matplotlib.pyplot as plt
+    # Create NetworkX graph
+    graph = nx.Graph()
+
+    # Add edges to the graph
+    for u in range(N):
+        for w, v in G[u]:
+            graph.add_edge(u, v, weight=w)
+
+    # Set up the plot
+    plt.figure(figsize=(12, 8))
+
+    # Create a layout for the graph
+    pos = nx.spring_layout(graph)
+
+    # Draw the graph
+    nx.draw_networkx_nodes(graph, pos, node_color='lightblue', node_size=500)
+    nx.draw_networkx_edges(graph, pos)
+    nx.draw_networkx_labels(graph, pos)
+
+    # Draw edge labels
+    edge_labels = nx.get_edge_attributes(graph, 'weight')
+    nx.draw_networkx_edge_labels(graph, pos, edge_labels=edge_labels)
+
+    # Set title and display the plot
+    plt.title("Graph Visualization")
+    plt.axis('off')
+    plt.tight_layout()
+    plt.show()
