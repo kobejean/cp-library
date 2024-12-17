c326e39a8a23ef6bce1b186d212245bb3925e12e
Jean Flaherty <kobejean@me.com>
2024-12-17 20:24:43 +0900

    update

---
+++ .verify-helper/cache/a6363e566203bcb5a6ac58b06d173aca/compiled.py
@@ -0,0 +1,20 @@
+#!/Users/kobejean/miniforge3/envs/atcoder/bin/pypy
+"""This is a helper script to run the target Python code.
+
+We need this script to set PYTHONPATH portably. The env command, quoting something, etc. are not portable or difficult to implement.
+"""
+
+import os
+import sys
+
+# arguments
+path = '/Users/kobejean/Developer/GitHub/cp-library/test/unionfind_with_potential_non_commutative_group.test.py'
+basedir = '/Users/kobejean/Developer/GitHub/cp-library'
+
+# run test/unionfind_with_potential_non_commutative_group.test.py
+env = dict(os.environ)
+if "PYTHONPATH" in env:
+    env["PYTHONPATH"] = basedir + os.pathsep + env["PYTHONPATH"] 
+else:
+    env["PYTHONPATH"] = basedir  # set `PYTHONPATH` to import files relative to the root directory
+os.execve(sys.executable, [sys.executable, path], env=env)  # use `os.execve` to avoid making an unnecessary parent process
