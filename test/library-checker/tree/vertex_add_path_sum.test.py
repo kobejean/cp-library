42b75085d7a18b094a3917f5359424773bb52e8e
Jean Flaherty <kobejean@me.com>
2025-03-28 21:58:31 +0900

    fix test

--- test/library-checker/tree/vertex_add_path_sum.test.py
+++ test/library-checker/tree/vertex_add_path_sum.test.py
@@ -1,18 +1,12 @@
 # verification-helper: PROBLEM https://judge.yosupo.jp/problem/vertex_add_path_sum
-
-from cp_library.ds.parallel_cls import Parallel
-
-
 def main():
-    mod = 998244353
     N, Q = read()
-    A, B = read(Parallel[N])
+    A = read(list[int])
     T = read(Tree[N,0])
     hld = HLD(T)
-    S = [0]*N
+    B = [0]*N
     for u in range(N):
-        t = hld.tin[u]
-        S[t] = A[u]*mod+B[u]
+        B[hld.tin[u]] = A[u]
     bit = BIT(B)
     ans = 0
     def query(l, r):
